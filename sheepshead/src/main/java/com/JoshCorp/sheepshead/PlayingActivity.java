9c69840b26cd2d66fec98de5f3372a0d3f21f151
kasuboski <kasuboski@wisc.edu>
2014-05-24 20:54:17 +0200

    Initial commit currently shows a random card when a card is clicked

---
+++ sheepshead/src/main/java/com/JoshCorp/sheepshead/PlayingActivity.java
@@ -0,0 +1,85 @@
+package com.JoshCorp.sheepshead;
+
+import android.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+
+import java.util.Random;
+
+
+public class PlayingActivity extends ActionBarActivity {
+
+    private final static int[] cardDeck = new int[] {
+            R.drawable.clubs_10,
+            R.drawable.clubs_9,
+            R.drawable.clubs_8,
+            R.drawable.clubs_7,
+            R.drawable.clubs_a,
+            R.drawable.clubs_j,
+            R.drawable.clubs_q,
+            R.drawable.clubs_k,
+            R.drawable.diamonds_10,
+            R.drawable.diamonds_9,
+            R.drawable.diamonds_8,
+            R.drawable.diamonds_7,
+            R.drawable.diamonds_a,
+            R.drawable.diamonds_k,
+            R.drawable.diamonds_q,
+            R.drawable.diamonds_j,
+            R.drawable.hearts_10,
+            R.drawable.hearts_9,
+            R.drawable.hearts_8,
+            R.drawable.hearts_7,
+            R.drawable.hearts_a,
+            R.drawable.hearts_k,
+            R.drawable.hearts_q,
+            R.drawable.hearts_j,
+            R.drawable.spades_10,
+            R.drawable.spades_9,
+            R.drawable.spades_8,
+            R.drawable.spades_7,
+            R.drawable.spades_a,
+            R.drawable.spades_k,
+            R.drawable.spades_q,
+            R.drawable.spades_j
+    };
+
+    private final static Random randy = new Random();
+
+    public void getCard(View view) {
+        view.setBackgroundResource(cardDeck[randy.nextInt(cardDeck.length)]);
+//        ImageButton card = (ImageButton)findViewById(view.getId());
+//        card.setBackgroundResource(cardDeck[randy.nextInt(cardDeck.length)]);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActionBar actionBar = getActionBar();
+        actionBar.hide();
+        setContentView(R.layout.activity_playing);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.playing, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ sheepshead/src/main/java/com/JoshCorp/sheepshead/WelcomeScreenActivity.java
@@ -0,0 +1,30 @@
+package com.JoshCorp.sheepshead;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+public class WelcomeScreenActivity extends Activity {
+
+    public final static String PLAYER_NAME = "com.JoshCorp.sheepshead.MESSAGE";
+
+    public void submitName(View view) {
+        Intent intent = new Intent(this, PlayingActivity.class);
+        EditText editText = (EditText) findViewById(R.id.player_name);
+        String message = editText.getText().toString();
+        intent.putExtra(PLAYER_NAME, message);
+        startActivity(intent);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActionBar actionBar = getActionBar();
+        actionBar.hide();
+        setContentView(R.layout.activity_welcome_screen);
+    }
+}
