04133ca9aa2c6e51b94d51ad8c7629fd06c8c71f
kasuboski <kasuboski@wisc.edu>
2014-05-25 12:57:47 +0200

    Added the cards in code instead of in the xml.

---
+++ sheepshead/src/main/java/com/JoshCorp/sheepshead/Hand.java
@@ -0,0 +1,7 @@
+package com.JoshCorp.sheepshead;
+
+/**
+ * Created by Marvin on 5/25/14.
+ */
+public class Hand {
+}
--- sheepshead/src/main/java/com/JoshCorp/sheepshead/PlayingActivity.java
+++ sheepshead/src/main/java/com/JoshCorp/sheepshead/PlayingActivity.java
@@ -6,11 +6,15 @@ import android.graphics.Color;
 import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewManager;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -64,8 +68,8 @@ public class PlayingActivity extends ActionBarActivity {
         //move card object
         Card card = (Card)view.getTag();
 
-        //clear image background
-        view.setBackgroundColor(Color.TRANSPARENT);
+        //remove imageButton of card
+        ((ViewManager)view.getParent()).removeView(view);
 
         //displayed random new card
 //        view.setBackgroundResource(cardDeck[randy.nextInt(cardDeck.length)]);
@@ -83,14 +87,42 @@ public class PlayingActivity extends ActionBarActivity {
         TextView playerName = (TextView)findViewById(R.id.PlayerName);
         playerName.setText(message);
 
-        //TODO: Add the cards programmatically
+        //get the layout
+        RelativeLayout layout = (RelativeLayout)findViewById(R.id.handContainer);
+
         //create a deck and deal cards
         Deck deck = new Deck();
         for(int i=1;i<=10;i++){
             Card card = deck.draw();
-            ImageButton image = (ImageButton)findViewById(getResources().getIdentifier("imageButton" + i , "id", getPackageName()));
+            ImageButton image = new ImageButton(this);
+            RelativeLayout.LayoutParams params =
+                    new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
+                            RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+            image.setId(i);
+            if(i == 2){
+                params.addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE);
+                params.addRule(RelativeLayout.ALIGN_PARENT_START,RelativeLayout.TRUE);
+                params.setMargins(75,0,0,0);
+            }
+            else if(i != 1){
+                params.addRule(RelativeLayout.RIGHT_OF,i-2);
+            }
+            params.addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE);
+            image.setLayoutParams(params);
+            image.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    getCard(v);
+                }
+            });
+            image.setAdjustViewBounds(true);
+            image.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            image.setMinimumWidth(150);
+            image.setMinimumHeight(200);
             image.setTag(card);
             image.setBackgroundResource(getResources().getIdentifier(card.getResource() , "drawable", getPackageName()));
+            layout.addView(image);
         }
     }
 }
